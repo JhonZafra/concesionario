f07666f9a8b5a35df01f0ff1b388e7918f8b8554
JhonZafra <jezortiz26@gmail>
2022-05-16 11:42:11 -0500

    Reorganizando el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "styles/styles.css";
-import Login from "pages/Login.jsx";
-import Registro from "pages/Registro.jsx";
+import Login from "pages/auth/Login.jsx";
+import Registro from "pages/auth/Registro";
 import AuthLayaout from "layouts/AuthLayaout";
 import PrivateLayaout from "layouts/PrivateLayaout";
 import Admin from "pages/admin/Index";
 import PublicLayout from "layouts/PublicLayout";
-import Index from "pages/Index";
+import Index from "pages/auth/Index";
 import Clientes from "pages/admin/Clientes";
 import Vehiculos from "pages/admin/Vehiculos";
 
@@ -16,8 +16,8 @@ function App() {
       <Routes>
         <Route element={<PrivateLayaout />}>
           <Route path="admin" element={<Admin />} />
-          <Route path="vehiculos" element={<Vehiculos />} />
-          <Route path="clientes" element={<Clientes />} />
+          <Route path="admin/vehiculos" element={<Vehiculos />} />
+          <Route path="admin/clientes" element={<Clientes />} />
         </Route>
         <Route element={<AuthLayaout />}>
           <Route path="login" element={<Login />} />
