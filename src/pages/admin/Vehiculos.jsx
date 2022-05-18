5e2b2d1b70e7276ad8cca85133028356aaf9b4ac
JhonZafra <jezortiz26@gmail>
2022-05-18 11:20:29 -0500

    Uso de .map

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -4,9 +4,43 @@ import { useEffect, useState } from "react";
 // realizar un formulario que le puda al usuario su edad y muestre un mensaje
 // que diga si el usuario es mayor de edad o no
 
+const vehiculosBackend = [
+  {
+    nombre: "Corolla",
+    marca: "Toyota",
+    modelo: 2014,
+  },
+  {
+    nombre: "Sandero",
+    marca: "Renault",
+    modelo: 2015,
+  },
+  {
+    nombre: "Rav4",
+    marca: "Toyota",
+    modelo: 2019,
+  },
+  {
+    nombre: "Fiesta",
+    marca: "Ford",
+    modelo: 2017,
+  },
+  {
+    nombre: "Mazda 3",
+    marca: "Mazda",
+    modelo: 2016,
+  },
+];
+
 const Vehiculos = () => {
   const [mostrarTabla, setMostrarTabla] = useState(false);
+  const [vehiculos, setVehiculos] = useState([]);
   const [textoBoton, settextoBoton] = useState("Crear nuevo vehículo");
+
+  useEffect(() => {
+    // obtener lista de vehículos desde el frontend o desde la base de datos
+    setVehiculos(vehiculosBackend);
+  }, []);
 
   useEffect(() => {
     if (mostrarTabla) {
@@ -26,16 +60,27 @@ const Vehiculos = () => {
           onClick={() => {
             setMostrarTabla(!mostrarTabla);
           }}
-          className="text-white bg-indigo-500 p-5 rounded-full m-12 w-max self-center">
+          className="text-white bg-indigo-500 p-5 rounded-full m-12 w-max self-center"
+        >
           {textoBoton}
         </button>
       </div>
-      {mostrarTabla ? <TablaVehiculos /> : <FormularioCreacionVehiculos />}
+      {mostrarTabla ? (
+        <TablaVehiculos listaVehiculos={vehiculos} />
+      ) : (
+        <FormularioCreacionVehiculos />
+      )}
     </div>
   );
 };
 
-const TablaVehiculos = () => {
+const TablaVehiculos = ({ listaVehiculos }) => {
+  useEffect(() => {
+    console.log(
+      "Este es el listado de vehículos en el componete tabla",
+      listaVehiculos
+    );
+  }, [listaVehiculos]);
   return (
     <table>
       <thead>
@@ -45,24 +90,17 @@ const TablaVehiculos = () => {
           <th>Modelo del vehículo</th>
         </tr>
       </thead>
-        <tbody>
-          <tr>
-            <td>Corolla</td>
-            <td>Toyota</td>
-            <td>2014</td>
-          </tr>
-          <tr>
-            <td>Corolla</td>
-            <td>Toyota</td>
-            <td>2014</td>
-          </tr>
-          <tr>
-            <td>Corolla</td>
-            <td>Toyota</td>
-            <td>2014</td>
-          </tr>
-        </tbody>
-
+      <tbody>
+        {listaVehiculos.map((vehiculo) => {
+          return (
+            <tr>
+              <td>{vehiculo.nombre}</td>
+              <td>{vehiculo.marca}</td>
+              <td>{vehiculo.modelo}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
